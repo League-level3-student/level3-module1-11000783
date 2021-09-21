1efa129fe857e857190308d78c60cfd033b2564d
League Student <league@iMac-55.attlocal.net>
2021-09-20 18:26:18 -0700

    September 20

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -18,33 +18,48 @@ public class _03_TestMatchingBrackets {
 		assertFalse(doBracketsMatch("}{"));
 	}
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS
+	// A MATCHING CLOSING BRACKET
+	Stack<Character> bracke = new Stack<Character>();
+
 	private boolean doBracketsMatch(String brackets) {
-		
-		//1. Use a for loop to iterate through your brackets String 
 
-    			//2.  If the current character is an '{'
-
-        			//3.  Push an '{' onto the stack 
-
-    			//4.  else if the character is a '}'
-
-        			//6.  if the stack is empty (i.e. there is no matching opening bracket)
-
-            				//7.  return false 
-
-        			//5.  Pop a character from the stack
-
-
-		//8.  if the stack is not empty (i.e. there were too many opening brackets)
-
-    			//9. return false 
-
-		//10. else (i.e. everything matched correctly)
-
-    			//11. return true 
+		// 1. Use a for loop to iterate through your brackets String
+		for (int i = 0; i < brackets.length(); i++) {
+			char bracket = brackets.charAt(i);
+			if (bracket == '{') {
+				bracke.push('{');
+			}
+			if (bracket == '}') {
+				char character = bracke.pop();
+			}
+		}
+		if (false == bracke.isEmpty()) {
+			return false;
+		} else { 
 			return true;
-		
+			
+		}
+		// 2. If the current character is an '{'
+
+		// 3. Push an '{' onto the stack
+
+		// 4. else if the character is a '}'
+
+		// 6. if the stack is empty (i.e. there is no matching opening bracket)
+
+		// 7. return false
+
+		// 5. Pop a character from the stack
+
+		// 8. if the stack is not empty (i.e. there were too many opening brackets)
+
+		// 9. return false
+
+		// 10. else (i.e. everything matched correctly)
+
+		// 11. return true
+
 	}
 
 }
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,12 +1,21 @@
 package _04_HangMan;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class HangMan{
 	public static void main(String[] args) {
+	String numberofWords = JOptionPane.showInputDialog(null, "Pick a number between 1 and 10");
+	int number = Integer.parseInt(numberofWords);
+	
+	String secretWord = Utilities.readRandomLineFromFile("dictionary.txt");
+	
 		
-		JOptionPane.showInputDialog(null, "Pick a number between 1 and 10");
+	
+	
+	}
 	}
 	
 	
-}
+
