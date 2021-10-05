78170d4f4f7d702b6170a3bf6082f838d6ad85b6
League Student <league@iMac-55.attlocal.net>
2021-10-04 18:19:26 -0700

    Update HangMan.java

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,5 +1,7 @@
 package _04_HangMan;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 import java.util.Stack;
 
@@ -8,10 +10,10 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class HangMan {
+public class HangMan implements KeyListener {
+	Stack<String> words = new Stack<String>();
 
 	public static void main(String[] args) {
-		Stack<String> words = new Stack<String>();
 
 		String numberofWords = JOptionPane.showInputDialog(null, "Pick a number between 1 and 10");
 		int number = Integer.parseInt(numberofWords);
@@ -19,19 +21,50 @@ public class HangMan {
 		for (int i = 0; i < number; i++) {
 			words.push(secretWord);
 		}
+		
+		
+		new HangMan().run(); 
+	}
+	
+	void run() {
 		String firstword = words.pop();
 		int numberofletters = firstword.length();
 		String blanklabel = "";
 		for (int i = 0; i < numberofletters; i++) {
 			blanklabel += "_";
-		}
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		frame.add(panel);
 		frame.setVisible(true);
-		frame.addKeyListener(null);
-		JLabel letter1 = new JLabel(blanklabel);
-		panel.add(letter1);
+		frame.addKeyListener(this);
+		JLabel wordbank = new JLabel(blanklabel);
+		panel.add(wordbank);
+		
+		}
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		main.secretWord();
+		e.getKeyChar();
 
+		keyPressed(e);
+		for (int i = 0; i < secretWord.length(); i++) {
+		String key = secretWord.charAt(i);
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
+	
